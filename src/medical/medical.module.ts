f584c66db7b90b9fac1d93f067f14f7196b7d66a
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-07-03 00:38:44 -0300

    :sparkles: fix: adding injection depedency

--- src/medical/medical.module.ts
+++ src/medical/medical.module.ts
@@ -1,16 +1,24 @@
 // src/medical/medical.module.ts
-import { Module } from '@nestjs/common';
-import { PatientController } from './controllers/patient.controller';
-import { DoctorController } from './controllers/doctor.controller';
-import { AppointmentController } from './controllers/appointment.controller';
-import { PatientService } from './services/patient.service';
-import { AppointmentService } from './services/appointment.service';
-import { PrismaService } from 'src/prisma.service';
-import { DoctorService } from './services/doctor.service';
+import { Module } from "@nestjs/common";
+import { PatientController } from "./controllers/patient.controller";
+import { DoctorController } from "./controllers/doctor.controller";
+import { AppointmentController } from "./controllers/appointment.controller";
+import { PatientService } from "./services/patient.service";
+import { AppointmentService } from "./services/appointment.service";
+import { PrismaService } from "src/prisma.service";
+import { DoctorService } from "./services/doctor.service";
+import { UserService } from "./services/user.service";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [],
+  imports: [JwtModule],
   controllers: [PatientController, DoctorController, AppointmentController],
-  providers: [PrismaService, PatientService, DoctorService, AppointmentService],
+  providers: [
+    PrismaService,
+    PatientService,
+    DoctorService,
+    AppointmentService,
+    UserService,
+  ],
 })
 export class MedicalModule {}
