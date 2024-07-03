d4054912c612736886c3e23310a7cce74509fd70
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-07-03 00:38:13 -0300

    :sparkles: fix: adding injection depedency

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MedicalModule } from "./medical/medical.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [MedicalModule],
+  imports: [MedicalModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
